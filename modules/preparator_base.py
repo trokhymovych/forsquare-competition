b0d443dc32315de010e11d6d29f3272a7ea2d83f
OleksandrKosovan <sanya.kosovan@gmail.com>
2022-05-07 15:29:04 +0300

    URL features

--- modules/preparator_base.py
+++ modules/preparator_base.py
@@ -1,6 +1,7 @@
 import re
 from timeit import default_timer as timer
 from datetime import timedelta
+from urllib.parse import urlparse
 
 import pandas as pd
 
@@ -27,8 +28,51 @@ class BaseFeatures:
         end = timer()
         print("adress_number was genareted by", timedelta(seconds=end-start))
 
+    def parse_url_domain(self):
+        def parse_domain(url: str):
+            try:
+                return urlparse(url).netloc
+            except:
+                pass
+
+        start = timer()
+        self.train_df["domain"] = self.train_df["url"].apply(parse_domain)
+        end = timer()
+        print("domain was parsed by", timedelta(seconds=end-start))
+
+    def calc_url_features(self):
+        def get_www(url):
+            try:
+                if "www" in url:
+                    return 1
+                else:
+                    return 2
+            except:
+                return 0
+        
+
+        def get_protocol(url):
+            try:
+                if "https" in url:
+                    return 1
+                else:
+                    return 2
+            except:
+                return 0
+
+        start = timer()
+        self.train_df["is_www"] = self.train_df["url"].apply(get_www)
+        self.train_df["protocol"] = self.train_df["url"].apply(get_protocol)
+        end = timer()
+        print("URL features was calculated by", timedelta(seconds=end-start))
+
+
+
+
     def prepare(self):
         self.extract_adress_number()
+        self.parse_url_domain()
+        self.calc_url_features()
         return self.train_df
 
 
